e885d6a407c02b89056206a0c9b475045386b811
Ruxxia <luck.ruxia@gmail.com>
2024-09-11 19:07:38 +0700

    Update world
    add collision

--- main/worlds/maps/TX Plant.tsx
+++ main/worlds/maps/TX Plant.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="TX Plant" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="TX Plant.png" trans="ff00ff" width="512" height="512"/>
+ <tile id="66">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.69858" y="14.1352">
+    <properties>
+     <property name="collision" type="bool" value="true"/>
+    </properties>
+    <polygon points="0.170648,-0.656132 0.129728,11.148 13.5566,11.1917 13.1025,-0.655764"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="74">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.01599" y="2.64357">
+    <properties>
+     <property name="collision" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 0.0852765,20.1252 17.1406,20.3811 16.5436,0.170553"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="5.37543" y="16.2969" width="20.5631" height="5.71672">
+    <properties>
+     <property name="collision" type="bool" value="true"/>
+     <property name="z" type="int" value="1"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
--- main/worlds/maps/TX Props.tsx
+++ main/worlds/maps/TX Props.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="TX Props" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="TX Props.png" trans="ff00ff" width="512" height="512"/>
+ <tile id="39">
+  <properties>
+   <property name="z" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="z" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
